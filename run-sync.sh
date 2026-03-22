19d89a9d0f8b645dae697237126ac6f87009d13d
Joseph Solomon <joseph.solomon89@gmail.com>
2026-03-22 11:56:22 -0400

    Release v20

--- run-sync.sh
+++ run-sync.sh
@@ -4,6 +4,15 @@ ACCOUNT="$1"
 PASSWORD="$2"
 TARGET="$3"
 LOGFILE="/var/log/cron/${ACCOUNT}.log"
+LOCKFILE="/tmp/run-sync-${ACCOUNT}.lock"
+
+if [ -e "$LOCKFILE" ]; then
+  echo "[$(date)] Skipping: previous sync for $ACCOUNT still running ($(cat $LOCKFILE))" >> "$LOGFILE"
+  exit 0
+fi
+
+echo $$ > "$LOCKFILE"
+trap 'rm -f "$LOCKFILE"' EXIT
 
 rotate "$LOGFILE"
 
